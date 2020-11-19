76dde0592db787202c7124506014c95b74c3bb77
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2020-11-19 12:14:13 +0530

    Optimized imports and added docstrings
    Signed-off-by: t0xic0der <akashdeep.dhar@gmail.com>

--- servdocs.py
+++ servdocs.py
@@ -1,5 +1,31 @@
+#!/usr/bin/env python
+
+"""
+##########################################################################
+*
+* -*- coding: utf-8 -*-
+*
+*   Copyright © 2019-2020 Akashdeep Dhar <t0xic0der@fedoraproject.org>
+*
+*   This copyrighted material is made available to anyone wishing to use,
+*   modify, copy, or redistribute it subject to the terms and conditions
+*   of the GNU General Public License v.2, or (at your option) any later
+*   version.  This program is distributed in the hope that it will be
+*   useful, but WITHOUT ANY WARRANTY expressed or implied, including the
+*   implied warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*   PURPOSE.  See the GNU General Public License for more details.  You
+*   should have received a copy of the GNU General Public License along
+*   with this program; if not, write to the Free Software Foundation,
+*   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*
+##########################################################################
+"""
+
+
+import click
+import json
+import time
 from flask import Flask, render_template, request, jsonify
-import click, time, json
 
 
 servchat = Flask(__name__)
