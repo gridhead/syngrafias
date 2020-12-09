1227e81917c85611bb303cad4a41ccb66a30940a
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2020-12-09 14:43:51 +0530

    Updated licensing information inside codes
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- syngrafias.py
+++ syngrafias.py
@@ -3,20 +3,20 @@
 """
 ##########################################################################
 *
-* -*- coding: utf-8 -*-
-*
 *   Copyright © 2019-2020 Akashdeep Dhar <t0xic0der@fedoraproject.org>
 *
-*   This copyrighted material is made available to anyone wishing to use,
-*   modify, copy, or redistribute it subject to the terms and conditions
-*   of the GNU General Public License v.2, or (at your option) any later
-*   version.  This program is distributed in the hope that it will be
-*   useful, but WITHOUT ANY WARRANTY expressed or implied, including the
-*   implied warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-*   PURPOSE.  See the GNU General Public License for more details.  You
-*   should have received a copy of the GNU General Public License along
-*   with this program; if not, write to the Free Software Foundation,
-*   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *
 ##########################################################################
 """
@@ -29,7 +29,7 @@ from modules import servdocs, webesock
 
 
 @click.command()
-@click.option("-s", "--servport", "servport", help="Set the port value for Syngrafias [0-65536]", required=True)
+@click.option("-s", "--servport", "servport", help="Set the port value for Interface [0-65536]", required=True)
 @click.option("-c", "--sockport", "sockport", help="Set the port value for WebSockets [0-65536]", required=True)
 @click.option("-6", "--ipprotv6", "netprotc", flag_value="ipprotv6", help="Start the server on an IPv6 address", required=True)
 @click.option("-4", "--ipprotv4", "netprotc", flag_value="ipprotv4", help="Start the server on an IPv4 address", required=True)
